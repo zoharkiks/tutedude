0af6e80fed1d9080039ee9e01b23e8fd82f490d1
Zohar <zoharkiks@gmail.com>
2022-11-15 12:59:19 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="font-poppins">
+      <h1 className=" text-heading">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      white: "#ffffff",
+      black: "#000",
+      purple: "#800080",
+      beige: "#EEAEAE",
+      gray: "#4D4D4D",
+    },
+   
+    extend: {
+      fontSize:{
+        heading:['32px',{lineHeight:'48px',fontWeight:500}],
+        heading2:['24px',{lineHeight:'36px',fontWeight:600}],
+        heading3:['20px',{lineHeight:'30px',fontWeight:500,letterSpacing:10}],
+        heading4:['16px',{lineHeight:'24px',fontWeight:500}],
+        paragraph:['16px',{lineHeight:'24px',fontWeight:400}]
+        
+            },
+
+    },
+  },
+  plugins: [],
+};
